157840ab21dbb7687c80d7168b90dc1738d2891f
SanaeProject <66119174+ubproject@users.noreply.github.com>
2021-10-08 21:31:09 +0900

    version.1 uploaded

---
+++ GmailSend/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace GmailSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.address = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.toaddres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.titlebox = new System.Windows.Forms.TextBox();
+            this.contents = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(12, 56);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(243, 19);
+            this.address.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "・メールアドレス";
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(274, 56);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(202, 19);
+            this.pass.TabIndex = 3;
+            // 
+            // toaddres
+            // 
+            this.toaddres.Location = new System.Drawing.Point(12, 106);
+            this.toaddres.Name = "toaddres";
+            this.toaddres.Size = new System.Drawing.Size(243, 19);
+            this.toaddres.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "・送信先メールアドレス(追加)";
+            // 
+            // titlebox
+            // 
+            this.titlebox.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.titlebox.Location = new System.Drawing.Point(12, 200);
+            this.titlebox.Name = "titlebox";
+            this.titlebox.Size = new System.Drawing.Size(243, 21);
+            this.titlebox.TabIndex = 6;
+            this.titlebox.Text = "タイトル";
+            // 
+            // contents
+            // 
+            this.contents.Location = new System.Drawing.Point(15, 241);
+            this.contents.Multiline = true;
+            this.contents.Name = "contents";
+            this.contents.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.contents.Size = new System.Drawing.Size(464, 168);
+            this.contents.TabIndex = 7;
+            this.contents.Text = "内容";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "送信する";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.linkLabel1.Location = new System.Drawing.Point(156, 417);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(63, 14);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "詳細情報";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel2.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(150)));
+            this.linkLabel2.Location = new System.Drawing.Point(155, 431);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(100, 23);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "help";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(10, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Program version:1+α";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(365, 445);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "(C) 2021 SanaeProject.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "上記のメールアドレスを送信先リストへ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(274, 106);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(202, 88);
+            this.checkedListBox1.TabIndex = 14;
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.linkLabel3.Location = new System.Drawing.Point(271, 87);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(96, 16);
+            this.linkLabel3.TabIndex = 15;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "・送信先リスト";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(12, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "・タイトル";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(15, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "・本文";
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Font = new System.Drawing.Font("MS UI Gothic", 11F);
+            this.linkLabel4.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel4.Location = new System.Drawing.Point(373, 88);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(88, 15);
+            this.linkLabel4.TabIndex = 18;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "リストから消す";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.linkLabel5.Location = new System.Drawing.Point(271, 37);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(111, 16);
+            this.linkLabel5.TabIndex = 19;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "・アプリパスワード";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 466);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.contents);
+            this.Controls.Add(this.titlebox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.toaddres);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.address);
+            this.Name = "Form1";
+            this.Text = "GmailSender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.TextBox toaddres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox titlebox;
+        private System.Windows.Forms.TextBox contents;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+    }
+}
+
---
+++ GmailSend/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace GmailSend
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        //メールアドレスのリスト
+        public static ArrayList members = new ArrayList();
+        //送信先リストの更新
+        public void load()
+        {
+            members.Clear();
+            System.IO.StreamReader sr2 = new System.IO.StreamReader(@"members.data", Encoding.GetEncoding("Shift_JIS"));
+            string str2 = sr2.ReadToEnd();
+            sr2.Close();
+            string[] str3 = str2.Split(',');
+            for (int i = 0; i < str3.Length - 1; i++)
+            {
+                str3[i].Replace("\r\n", "");
+                members.Add(str3[i]);
+            }
+            checkedListBox1.Items.Clear();
+            for (int g = 0; g < members.Count; g++)
+            {
+                checkedListBox1.Items.Add(members[g]);
+            }
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+        //Main
+        public Form1()
+        {
+            InitializeComponent();
+            //以前使ったパスワードの復元
+            if (System.IO.File.Exists(@"pass.data"))
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(@"pass.data", Encoding.GetEncoding("Shift_JIS"));
+                string str = sr.ReadToEnd();
+                sr.Close();
+                pass.Text = str;
+            }
+            if (System.IO.File.Exists(@"members.data"))
+            {
+                load();
+            }
+            else
+            {
+                Encoding enc = Encoding.GetEncoding("Shift_JIS");
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(@"members.data", false, enc);
+                writer.Write("");
+                writer.Close();
+            }
+        }
+        //送信ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (address.Text == "" || pass.Text == "" || toaddres.Text == "" || titlebox.Text == "" || contents.Text == "")
+                {
+                    MessageBox.Show("内容が不足しています。", "警告");
+                }
+                else
+                {
+                    if (checkedListBox1.CheckedIndices.Count == 0)
+                    {
+                        MessageBox.Show("送信する人がいません。\r\nチェックリストで一人以上チェックが入っていることを確認してください。","警告");
+                    }
+                    for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
+                    {
+                        SmtpClient smtp = new SmtpClient();
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.Credentials = new System.Net.NetworkCredential(address.Text, pass.Text);
+                        smtp.EnableSsl = true;
+                        MailMessage msg = new MailMessage(address.Text, checkedListBox1.CheckedItems[i].ToString(), titlebox.Text, contents.Text);
+                        smtp.Send(msg);
+                        Encoding enc = Encoding.GetEncoding("Shift_JIS");
+                        System.IO.StreamWriter writer = new System.IO.StreamWriter(@"pass.data", false, enc);
+                        writer.WriteLine(pass.Text);
+                        writer.Close();
+                    }
+                    MessageBox.Show("全員への送信が完了しました。\r\n計:" + checkedListBox1.CheckedIndices.Count + "人", "報告");
+
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message, "例外的なエラー");
+            }
+        }
+        //詳細情報
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Version:1+α\n開発言語:C#\nEditer:Visual Studio2019\nAuthor:SanaeProject.\n参考サイト:https://qiita.com/R_TES_/items/d746e0e8197ce36a14fe","詳細");
+        }
+        //help
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://ublog.web.fc2.com/help/?program=GS");
+        }
+        //送信先リストへ
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(@"members.data", Encoding.GetEncoding("Shift_JIS"));
+            string str2 = sr.ReadToEnd();
+            sr.Close();
+            if (-1 != str2.IndexOf(toaddres.Text))
+            {
+                MessageBox.Show("同じメールアドレスが既に登録されています。", "警告");
+            }
+            else
+            {
+                Encoding enc = Encoding.GetEncoding("Shift_JIS");
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(@"members.data", false, enc);
+                writer.Write(str2 + toaddres.Text + ",");
+                writer.Close();
+                MessageBox.Show(toaddres.Text + "を追加しました。");
+                toaddres.Text = "";
+                load();
+            }
+        }
+        //送信先リスト
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("送信先リストは\"チェックされている\"メールアドレスに送信します。\r\nそのため送りたくない人にはチェックを外してください。","説明");
+        }
+        bool cleanmode = false;
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (!cleanmode)
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    checkedListBox1.SetItemChecked(i, false);
+                }
+                MessageBox.Show("消したいメールアドレスをチェックしてください。", "操作");
+                cleanmode = true;
+                linkLabel4.Text = "消去する";
+            }
+            else
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(@"members.data", Encoding.GetEncoding("Shift_JIS"));
+                string str = sr.ReadToEnd();
+                sr.Close();
+                for (int s = 0; s < checkedListBox1.CheckedIndices.Count; s++)
+                {
+                    str =str.Replace(checkedListBox1.CheckedItems[s] + ",", "");
+                    Encoding enc = Encoding.GetEncoding("Shift_JIS");
+                    System.IO.StreamWriter writer = new System.IO.StreamWriter(@"members.data", false, enc);
+                    writer.Write(str);
+                    writer.Close();
+                }
+                MessageBox.Show("消去完了しました。", "報告");
+                cleanmode = false;
+                linkLabel4.Text = "リストから消す";
+                load();
+            }
+        }
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://support.google.com/accounts/answer/185833?hl=ja");
+        }
+    }
+}
